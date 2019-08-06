cd761088f0681306a918738b6511783e44166d32
SamratMachinesense <sachakraborty@machinesense.com>
2019-08-06 05:57:48 +0000

    disintegration to speedtest.py

--- speedTest.js
+++ speedTest.js
@@ -1,77 +1,41 @@
-var speedTest = require('speedtest-net');
 var mqtt = require('mqtt');
-var config = require('./speedTest_config.json');
-var client  = mqtt.connect(config.mqtt_broker);
-var exec = require('child_process').execSync;
+var config = require('/usr/src/conf/speedTest_config.json');
+var execSync = require('child_process').execSync;
+var exec = require('child_process').exec;
 
-var mac = exec('cat /sys/class/net/eth0/address').toString().substring(0,17);
 
+var client = mqtt.connect(config.mqtt_broker);
+
+//var mac = execSync('cat /sys/class/net/eth0/address').toString().substring(0,17);
+var mac = "b8:27:eb:30:05:d8";
 console.dir(config);
-client.on('connect',function(){
-	console.log("Successfully connceted to " + config.mqtt_broker);
-	try{
-		setInterval(function(){
-			var test = speedTest({maxTime: (config.max_time_out || config.interval)}); 
-			test.on('data',function(data){
-				//console.dir(data);
-				try{
-					client.publish("info/heartbeat/"+mac+"/net_dwnld", data.speeds.download.toString());
-					console.log("Publishing "+"info/heartbeat/"+mac+"/net_dwnld  => "+ data.speeds.download.toString());
-					client.publish("info/heartbeat/"+mac+"/net_upld", data.speeds.upload.toString());
-					console.log("Publishing "+"info/heartbeat/"+mac+"/net_upld  => "+ data.speeds.upload.toString());
-				}
-				catch(exception){
-					console.error("Error while publishing data " + exception);
-					console.log(exception.stack);
-				}
-			});
-
-			test.on('error', function(err) {
-				console.error("Error while connecting to speedtest.net");
-				client.publish("info/heartbeat/"+config.mac+"/net_dwnld", "0");
-				client.publish("info/heartbeat/"+config.mac+"/net_upld", "0");
-			 	console.error(err);
-			});
-		},(config.interval || config.max_time_out));
-	}
-	catch(exception){
-		 console.log("something went wrong !", exception);
-	}
-});/*
-
-
-
-
- 
-test.on('data', data => {
-  console.dir(data);
-});
- 
-test.on('error', err => {
-  console.error(err);
-});
-
-function edgeMqttSocket(config){
-    var client  = mqtt.connect(config.url);
-    console.log("initializing socket connection with "+ config.url);
-    client.on('connect',function(){
-        try{
+	client.on('connect',function(){
+		console.log("Successfully connceted to " + config.mqtt_broker);
+		try{
             setInterval(function(){
-                var input = fs.readFileSync(config.input_data).toString();
-                console.log(JSON.parse(input));
-                //var machineId = (JSON.parse(input)['header'] || {} )['machineId'] || "";
-                //console.log(machineId);
-                var output = fs.readFileSync(config.output_data).toString();
-                console.log(JSON.parse(output));
-                var time  = moment().tz("America/New_York").format('YYYY-MM-DDTHH:mm:ss.SSSZZ');
-                client.publish('edge/input@@'+config.input_data.split('/').pop(),input);
-                client.publish('edge/output@@'+config.output_data.split('/').pop(),output);
-                console.log(`${time} : publishing ${config.input_data}, ${config.output_data} file to ${config.ur}l}`);
-            },( config.Interval || 30000 ))
+            exec('/usr/src/conf/speedtest.py --simple',(err, stdout, stderr)=>{
+                if(stdout){
+                    var str = stdout.toString();
+                    //console.log(str);
+                    var temp1 = str.split('\n');
+                    //console.dir(temp1);
+                    var dwnldSpd = temp1[1].split(':')[1].replace(" Mbit/s","");
+                    var upldSpd = temp1[2].split(':')[1].replace(" Mbit/s","");
+                    console.log("dwnldSpd "+dwnldSpd+" upLoad "+upldSpd);
+                    client.publish("info/heartbeat/"+mac+"/net_dwnld", dwnldSpd);
+                    client.publish("info/heartbeat/"+mac+"/net_upld", upldSpd);
+		    console.log("Published "+ "dwnldSpd "+dwnldSpd+" upLoad "+upldSpd);
+                }
+                else if(stderr){
+                    console.error('Error is running speed.py'+stderr);
+                }
+                else{
+                    console.log("something went wrong !",err)
+                }
+            });
+            },1000*60);
         }
-        catch(exception){
-            console.log("something went wrong !", exception);
-        }
-    })
-
-}*/
+		catch(exception){
+			console.log("something went wrong !", exception);
+		}
+	});
